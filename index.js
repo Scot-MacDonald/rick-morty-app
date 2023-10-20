f4bebdbe76bc0254795af143b7d9b6f6e2a2552b
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-20 11:40:29 +0200

    card

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+const url = "https://rickandmortyapi.com/api/character";
+
+async function fetchDataAndRender() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    // createAndRenderCards(data.results);
+
+    console.log(data);
+  } catch (error) {
+    console.error("An Error occurred");
+  }
+}
+// // let body = document.querySelector("body");
+// // function createAndRenderCards(results) {
+// //   results.forEach((person) => {
+// //     const card = Card(person);
+// //     body.append(card);
+// //   });
+// }
+
+fetchDataAndRender();
