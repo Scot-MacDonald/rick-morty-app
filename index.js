86f924a5e87df7da1ea83c82417da66edd23ad5e
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-20 16:48:03 +0200

    next page button

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // );
 // const searchBar = document.querySelector('[data-js="search-bar"]');
 // const navigation = document.querySelector('[data-js="navigation"]');
-// const prevButton = document.querySelector('[data-js="button-prev"]');
+const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 // const pagination = document.querySelector('[data-js="pagination"]');
 
