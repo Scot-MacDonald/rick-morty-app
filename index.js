d962e96f35c50916415dba4601efd320ed7df935
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-22 15:20:27 +0200

    search bar is functioning

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const cardContainer = document.querySelector('[data-js="card-container"]');
-// const searchBarContainer = document.querySelector(
-//   '[data-js="search-bar-container"]'
-// );
-// const searchBar = document.querySelector('[data-js="search-bar"]');
+const searchBarContainer = document.querySelector(
+  '[data-js="search-bar-container"]'
+);
+const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
+const inputvalue = searchBar.querySelector(".search-bar__input").value;
 
+console.log("aquii", inputvalue);
 //States
 const maxPage = 42;
 let page = 1;
-// const searchQuery = "";
+let searchQuery = "";
 
 import createCharacterCard from "./components/card/card.js";
 
-async function fetchDataAndRender(counter) {
-  const url = `https://rickandmortyapi.com/api/character?page=${counter}`;
+async function fetchDataAndRender(counter, searchQuery = "") {
+  const url = `https://rickandmortyapi.com/api/character/?name=${searchQuery}`;
+
   try {
     const response = await fetch(url);
     const data = await response.json();
@@ -36,6 +39,7 @@ async function fetchDataAndRender(counter) {
       setTimeout(() => {
         scrollToTop();
       }, 100);
+      searchBar.querySelector(".search-bar__input").value = "";
     } else {
       console.error("No results found in the API response");
     }
@@ -67,6 +71,18 @@ nextButton.addEventListener("click", () => {
 prevButton.addEventListener("click", () => {
   if (page > 1) {
     fetchDataAndRender(page - 1);
+  }
+});
+
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchQuery = searchBar.querySelector(".search-bar__input").value;
+  console.log(searchQuery);
+  if (searchQuery.trim() !== "") {
+    page = 1;
+    fetchDataAndRender(page, searchQuery);
+  } else {
+    console.log("Please enter a character name for the search.");
   }
 });
 
