c300d61554c8ad012bf7da101104decaeaa70c95
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-22 20:44:19 +0200

    search with pagination

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const cardContainer = document.querySelector('[data-js="card-container"]');
-// const searchBarContainer = document.querySelector(
-//   '[data-js="search-bar-container"]'
-// );
-// const searchBar = document.querySelector('[data-js="search-bar"]');
+const searchBarContainer = document.querySelector(
+  '[data-js="search-bar-container"]'
+);
+const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-//States
-const maxPage = 42;
+let maxPage = 42;
 let page = 1;
-// const searchQuery = "";
+let searchQuery = "";
 
 import createCharacterCard from "./components/card/card.js";
 
 async function fetchDataAndRender(counter) {
-  const url = `https://rickandmortyapi.com/api/character?page=${counter}`;
+  const url = `https://rickandmortyapi.com/api/character?page=${counter}&name=${searchQuery}`;
   try {
     const response = await fetch(url);
     const data = await response.json();
 
     if (data.results) {
-      const cardContainer = document.querySelector(".card-container");
       cardContainer.innerHTML = "";
 
       data.results.forEach((character) => {
@@ -31,27 +29,21 @@ async function fetchDataAndRender(counter) {
       });
 
       page = counter;
+
+      // Set maxPage based on the number of pages in the API response
+      if (data.info && data.info.pages) {
+        maxPage = data.info.pages;
+      }
+
       updatePagination();
 
-      setTimeout(() => {
-        scrollToTop();
-      }, 100);
+      document.body.scrollIntoView({ behavior: "smooth" });
     } else {
       console.error("No results found in the API response");
     }
   } catch (error) {
     console.error("An error occurred:", error);
   }
-}
-
-// let counter = 1;
-// nextButton.addEventListener("click", () => {
-//   ++counter;
-//   fetchDataAndRender(counter);
-// });
-
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
 function updatePagination() {
@@ -68,6 +60,11 @@ prevButton.addEventListener("click", () => {
   if (page > 1) {
     fetchDataAndRender(page - 1);
   }
+});
+
+searchBar.addEventListener("input", (event) => {
+  searchQuery = event.target.value.toLowerCase();
+  fetchDataAndRender(1);
 });
 
 fetchDataAndRender(page);
